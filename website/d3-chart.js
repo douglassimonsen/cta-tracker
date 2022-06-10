1eec4ba66d45cd3bfd3caa3d5ed0e7e5bb29a204
Matthew Hamilton <mwhamilton6@gmail.com>
2022-06-10 16:01:48 -0500

    Spacing stations properly

--- website/d3-chart.js
+++ website/d3-chart.js
@@ -1,7 +1,7 @@
 const stations = [{dist: 0, name: "matt"}, {dist: 1, name: "william"}, {dist: 30, name: "hamilton"}];
 const day = '2022-06-10';
 const MARGIN = {
-  left: 20,
+  left: 60,
   right: 20,
   top: 20,
   bottom: 20
@@ -19,15 +19,21 @@ function initialize(){
       .style("margin", "20px");
   
   const yScale = d3.scaleLinear().domain(d3.extent(stations, (d) => d.dist)).range([SIZE.height - MARGIN.bottom, MARGIN.top]);
-  body.append("g").attr("transform", "translate(20, 0)").call(d3.axisLeft(yScale));
+  body.append("g")
+      .attr("transform", `translate(${MARGIN.left}, 0)`)
+      .call(d3.axisLeft(yScale)
+              .tickValues(stations.map(x => x.dist))
+              .tickFormat(function(d, i){return stations[i].name})
+      );
   const dayParsed = new Date(day + 'T00:00:00');
   const xScale = d3.scaleTime().domain([
     new Date(+dayParsed - 1000 * 60 * 60 * 3),
     new Date(+dayParsed + 1000 * 60 * 60 * 27), // +- 3 hours around the date
   ]).range([MARGIN.left, SIZE.width - MARGIN.right]);
   body.append("g").attr("transform", `translate(0, ${SIZE.height - MARGIN.bottom})`).call(d3.axisBottom(xScale));
-  debugger;
 }
 
 
-initialize();
+const chart = {
+  initialize: initialize,
+};
