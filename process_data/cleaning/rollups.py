74de30aab12fc5ee013c918275fd3cfbf19e09bb
Matthew Hamilton <mwhamilton6@gmail.com>
2022-06-27 17:11:11 -0500

    Rollups now have a historical recalc function to quickly rerun all the data.

--- process_data/cleaning/rollups.py
+++ process_data/cleaning/rollups.py
@@ -6,8 +6,9 @@ s3_bucket = boto3.resource("s3").Bucket('cta-bus-and-train-tracker')
 s3_client = boto3.client("s3")
 
 
-def rollup(folder: str):
-    today = (datetime.datetime.utcnow() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+def rollup(folder: str, today: str=None):
+    if today is None:
+        today = (datetime.datetime.utcnow() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
     ret = []
     for obj in s3_bucket.objects.filter(Prefix=f'{folder}/{today}'):
         resp = s3_client.get_object(Bucket='cta-bus-and-train-tracker', Key=obj.key)['Body'].read()
@@ -24,6 +25,13 @@ def rollup(folder: str):
         Key=f'{folder}/rollup/{today}.bz2',
     )
 
+
+def historical_recalc(folder):
+    day_dict = {}
+    days  = {x.key.split('/')[1] for x in s3_bucket.objects.filter(Prefix=folder)}
+    for d in sorted(days):
+        print(folder, d)
+        rollup(folder, d)
 
 def main():
     for x in ['traintracker', 'bustracker']:
