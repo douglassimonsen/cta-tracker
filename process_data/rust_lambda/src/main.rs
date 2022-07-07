cd6d7c00b95493da8d66cc7a07591e58cd36fcf2
Matthew Hamilton <daedalus1432@gmail.com>
2022-07-06 21:01:30 -0500

    Need to figure out ownership

--- process_data/rust_lambda/src/main.rs
+++ process_data/rust_lambda/src/main.rs
@@ -2,7 +2,7 @@ use serde::{Serialize, Deserialize};
 use std::{fs, collections::HashMap};
 use std::time::Duration;
 use soup::prelude::*;
-use chrono::{DateTime, Utc};
+use chrono::{Utc};
 
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -17,36 +17,41 @@ struct Route {
   stops: HashMap<String, Vec<Stop>>
 }
 
-fn get_stop_list() -> HashMap<String, Route> {
+fn get_stop_list() -> Vec<Stop> {
     // Some JSON input data as a &str. Maybe this comes from the user.
     let data = fs::read_to_string("C:/Users/mwham/Documents/repos/cta-tracker/process_data/fill_picklists/bus-stops.json").unwrap();
-    return serde_json::from_str(&data).unwrap();
+    let original_data: HashMap<String, Route> = serde_json::from_str(&data).unwrap();
+    let ret: Vec<&Stop> = original_data.values().flat_map(|x| {
+      let ret: Vec<&Stop> = x.stops.values().flat_map(|x| x).collect();
+      return ret;
+    }).collect();
+    return Vec::new();
 }
 
-fn get_stops(v: HashMap<String, Route>){
-  for (key, val) in v.into_iter() {
-    for (name, stops) in val.stops.into_iter() {
-
-    }
-  }
-}
-fn get_arrivals(stop_id: &str){
+fn get_arrivals(stop_id: &str) -> Vec<HashMap<String, String>>{
   let client = reqwest::blocking::Client::builder().timeout(Duration::from_secs(10)).build().unwrap();
   let response = client.get(
     format!("http://www.ctabustracker.com/bustime/eta/getStopPredictionsETA.jsp?route=all&stop={stop_id}", stop_id=stop_id)
   ).send().unwrap().text().unwrap();
   let soup = Soup::new(&response);
-  for stop in soup.tag("stop").find_all().into_iter() {
+  return soup.tag("stop").find_all().map(|stop | {
     let mut row: HashMap<String, String> = stop.tag("pre").find().unwrap().children().skip(1).filter(|x| x.name() != "[text]").map(|x| {
       return (x.name().to_string(), x.text());
     }).collect();
-    let now = Utc::now();
-    row.insert("response_at".to_string(), now.to_rfc3339());
+    row.insert("response_at".to_string(), Utc::now().to_rfc3339());
     row.insert("stop_id".to_string(), stop_id.to_string());
+    return row;
+  }).collect();
+}
+
+fn get_stops(v: Vec<Stop>){
+  let mut ret: Vec<HashMap<String, String>> = Vec::new();
+  for stop in v.into_iter(){
+    ret.extend(get_arrivals(&stop.id));
   }
 }
 
 fn main() {
-  get_stop_list();
-  get_arrivals("5981");
+  let stop_list = get_stop_list();
+  get_stops(stop_list);
 }
