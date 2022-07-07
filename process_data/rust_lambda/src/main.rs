8302ee641bd74d40a5c7a2dc5fa99cd3eb102ab4
Matthew Hamilton <daedalus1432@gmail.com>
2022-07-06 21:15:55 -0500

    now get_stop_list actually returns a Vec. Handling response edge cases.

--- process_data/rust_lambda/src/main.rs
+++ process_data/rust_lambda/src/main.rs
@@ -18,14 +18,16 @@ struct Route {
 }
 
 fn get_stop_list() -> Vec<Stop> {
-    // Some JSON input data as a &str. Maybe this comes from the user.
-    let data = fs::read_to_string("C:/Users/mwham/Documents/repos/cta-tracker/process_data/fill_picklists/bus-stops.json").unwrap();
-    let original_data: HashMap<String, Route> = serde_json::from_str(&data).unwrap();
-    let ret: Vec<&Stop> = original_data.values().flat_map(|x| {
-      let ret: Vec<&Stop> = x.stops.values().flat_map(|x| x).collect();
-      return ret;
-    }).collect();
-    return Vec::new();
+  let data = fs::read_to_string("C:/Users/mwham/Documents/repos/cta-tracker/process_data/fill_picklists/bus-stops.json").unwrap();
+  let original_data: HashMap<String, Route> = serde_json::from_str(&data).unwrap();
+  let ret: Vec<&Stop> = original_data.values().flat_map(|x| {
+    let ret: Vec<&Stop> = x.stops.values().flat_map(|x| x).collect();
+    return ret;
+  }).collect();
+  return ret.into_iter().map(|x| Stop{
+    id: x.id.to_string(), 
+    name: x.name.to_string()
+  }).collect();
 }
 
 fn get_arrivals(stop_id: &str) -> Vec<HashMap<String, String>>{
@@ -34,8 +36,9 @@ fn get_arrivals(stop_id: &str) -> Vec<HashMap<String, String>>{
     format!("http://www.ctabustracker.com/bustime/eta/getStopPredictionsETA.jsp?route=all&stop={stop_id}", stop_id=stop_id)
   ).send().unwrap().text().unwrap();
   let soup = Soup::new(&response);
-  return soup.tag("stop").find_all().map(|stop | {
-    let mut row: HashMap<String, String> = stop.tag("pre").find().unwrap().children().skip(1).filter(|x| x.name() != "[text]").map(|x| {
+  return soup.tag("stop").find_all().filter(|stop| !(stop.text().contains("No arrival times") || stop.text().contains("No service scheduled"))).map(|stop | {
+    let parent =  stop.tag("pre").find().unwrap();
+    let mut row: HashMap<String, String> = parent.children().skip(1).filter(|x| x.name() != "[text]").map(|x| {
       return (x.name().to_string(), x.text());
     }).collect();
     row.insert("response_at".to_string(), Utc::now().to_rfc3339());
@@ -47,6 +50,7 @@ fn get_arrivals(stop_id: &str) -> Vec<HashMap<String, String>>{
 fn get_stops(v: Vec<Stop>){
   let mut ret: Vec<HashMap<String, String>> = Vec::new();
   for stop in v.into_iter(){
+    println!("{:?}", stop);
     ret.extend(get_arrivals(&stop.id));
   }
 }
